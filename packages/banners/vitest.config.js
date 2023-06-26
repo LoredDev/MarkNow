afb5d3742e397e922be26fb04c8899787b87d54a
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-06-26 15:26:43 -0300

    test(banners): 🚨 remove edge test environments

--- packages/banners/vitest.config.js
+++ packages/banners/vitest.config.js
@@ -4,7 +4,6 @@ export default defineProject({
 	test: {
 		environmentMatchGlobs: [
 			['**\/*{,.node}.test.{js,ts}', 'node'],
-			['**\/*.edge.test.{js,ts}', 'edge-runtime'],
 		],
 		include: ['./tests/**/*.test.js'],
 	},
